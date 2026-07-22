d77a2f8eb610dfb582362d53245b9aee4ab890e3
jd8071 <jennifer.m.dahl@gmail.com>
2026-07-21 20:01:01 -0500

    Added Registration Features

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PPECheckoutSystem.Models;
+using PPECheckoutSystem.Data;
+
+namespace PPECheckoutSystem.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+
+        public RegistrationController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(user);
+        }
+    }
+}
